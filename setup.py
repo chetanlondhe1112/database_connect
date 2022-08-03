f229a924d2484e3e17542027b0f0a6e0a071da4e
chetan <chetanlondhe1112@gmail.com>
2022-08-03 18:50:09 +0530

    update 0.2 1) setup.py: added setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='database_connect',
+    version='0.1',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='Chetan Londhe',
+    author_email='chetanlondhe1112@gmail.com',
+    description='Stocks Fundamental anlyser'
+)
