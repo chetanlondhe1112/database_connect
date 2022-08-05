5a40b9f33851fbbe7c1239be5280cf8616733b86
chetan <chetanlondhe1112@gmail.com>
2022-08-05 20:06:13 +0530

    update 0.4 final for demo

--- Home.py
+++ Home.py
@@ -1,37 +1,10 @@
 import streamlit as st
-import pandas as pd
+import database as db
 from PIL import Image
 from datetime import datetime
 import streamlit_authenticator as stauth
-import mysql.connector
-from sqlalchemy import create_engine
+
 current_date = datetime.now()
-
-
-_ = """
-    to setup mysql connection Creating connection
-"""
-
-
-def init_msconnection():
-    return mysql.connector.connect(**st.secrets["mysql"])
-
-
-def init_sqconnection():
-    connect_string = "mysql://{}:@{}/{}".format(st.secrets["mysql"]["user"],
-                                                st.secrets["mysql"]["host"],
-                                                st.secrets["mysql"]["database"])
-    return create_engine(connect_string)
-
-
-conn = init_msconnection()
-sq_conn = init_sqconnection()
-cur = conn.cursor()
-
-admin = st.secrets["admin"]["admin"]
-master_table = st.secrets["db_table"]["master_table"]
-user_table = st.secrets["db_table"]["user_table"]
-query_table = st.secrets["db_table"]["query_table"]
 
 st.set_page_config(
      page_title="Stocks Analyser",
@@ -62,50 +35,45 @@ def main():
     process = st.sidebar.selectbox("Menu", Menu)
 
     if process == "Login":
-        df = pd.read_sql_query("SELECT * FROM " + user_table, conn)
-        names = list(df['id'])
-        usernames = list(df['username'])
-        hashed_passwords = list(df['password'])
+        users = db.fetch_all_users()
 
-        authenticator = stauth.Authenticate(names, usernames, hashed_passwords, "sfdb", "abcdef", cookie_expiry_days=30)
+        username = [user["key"] for user in users]
+        names = [user["name"] for user in users]
+        hashed_password = [user["password"] for user in users]
+
+        authenticator = stauth.Authenticate(names, username, hashed_password, "sfdb", "abcdef", cookie_expiry_days=30)
 
         names, st.session_state['authentication_status'], username = authenticator.login("Login", "main")
 
         if st.session_state["authentication_status"]:
-            st.write(f'Welcome *{st.session_state["username"]}*')
+            st.markdown(f'# Welcome *{st.session_state["username"]}*')
             authenticator.logout('Logout', 'sidebar')
-            st.session_state["username"]=username
-            if st.session_state["username"]==admin:
-                st.session_state["lable"]='admin'
-            else:
-                st.session_state["lable"]='user'
         elif st.session_state["authentication_status"] == False:
             st.error('Username/password is incorrect')
         elif st.session_state["authentication_status"] == None:
             st.warning('Please enter your username and password')
 
     elif process == "Sign Up":
+        users = db.fetch_all_users()
+        usernames = [user["key"] for user in users]
+        names = [user["name"] for user in users]
+
         st.subheader("Create New Account")
-        user_id = st.text_input("User ID (mail_id)")
+        name = st.text_input("name")
         username = st.text_input("Username")
         password = st.text_input("Password", type='password')
 
-
-        acc_query = "SELECT * FROM user_login where id='" + str(
-            user_id) + "' AND username='" + username + "' AND password='" + password + "'"
-        cur.execute(acc_query)
-        values = cur.fetchall()
         if st.button("Sign Up"):
-            if values:
+            if name in names and username in usernames:
                 st.warning("User already exist!")
             else:
-                hashed_password = stauth.Hasher([str(password)]).generate()
+                hashed_password = stauth.Hasher(password).generate()
                 # print(hashed_password[0])
-                add_query = 'insert into `user_login`(`id`,`username`,`password`)VALUES(%s,%s,%s)'
-                cur.execute(add_query, (str(user_id), username, str(hashed_password[0])))
-                conn.commit()
+                db.insert_user(username,name,hashed_password[0])
                 st.success("Successfully created.")
                 st.balloons()
+
+
 
 if __name__ == '__main__':
     main()
